b84131a7e5db5a1ca4257fa3a5869a9fc14ccae8
Muhammet Furkan ÖZTÜRK <m.furkanozturkk@gmail.com>
2021-11-21 01:21:19 +0300

    first

--- HesapMakinesi/ViewController.swift
+++ HesapMakinesi/ViewController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var firstNumber: UITextField!
+    @IBOutlet weak var secondNumber: UITextField!
+    @IBOutlet weak var resultNumber: UILabel!
+    var result = 0
+   
+    @IBAction func toplaFunc(_ sender: Any) {
+        if let numFirst = Int(firstNumber.text!){
+            if let numSecond = Int(secondNumber.text!){
+                result = numFirst + numSecond
+                resultNumber.text = String(result)
+            }
+        }
     }
+    @IBAction func cikarFunc(_ sender: Any) {
+        if let numFirst = Int(firstNumber.text!){
+            if let numSecond = Int(secondNumber.text!){
+                result = numFirst - numSecond
+                resultNumber.text = String(result)
+            }
+        }
+    }
+    @IBAction func carpFunc(_ sender: Any) {
+        if let numFirst = Int(firstNumber.text!){
+            if let numSecond = Int(secondNumber.text!){
+                result = numFirst * numSecond
+                resultNumber.text = String(result)
+            }
+        }
+    }
+    @IBAction func bolFunc(_ sender: Any) {
+        if let numFirst = Int(firstNumber.text!){
+            if let numSecond = Int(secondNumber.text!){
+                result = numFirst / numSecond
+                resultNumber.text = String(result)
+            }
+        }
+    }
+    
 
 
 }
